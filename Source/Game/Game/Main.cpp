0b0f2578f62c123c88f735614286e3a9cc24fd15
kwadas <kwadas@student.neumont.edu>
2023-07-03 11:07:44 -0600

    common property sheet added

--- Source/Game/Game/Main.cpp
+++ Source/Game/Game/Main.cpp
@@ -1,9 +1,46 @@
+#include "Core/Random.h"
+#include "Core/FileIO.h"
+#include "Core/Memory.h"
+#include "Core/Time.h"
+#include "Renderer/Renderer.h"
 #include <iostream>
+
+using namespace std;
+
+
+
 
 int main()
 {
-#ifdef _DEBUG
-	std::cout << "debug!\n"; 
-#endif 
-	std::cout << "hello world!\n";
+	kiko::g_memoryTracker.DisplayInfo();
+	int* p = new int;
+	kiko::g_memoryTracker.DisplayInfo();
+	delete p;
+	kiko::g_memoryTracker.DisplayInfo();
+
+	kiko::Time timer;
+	for (int i = 0; i < 1000000; i++) {}
+	cout << timer.GetElapsedNanoseconds() << endl;
+
+	/*auto start = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < 1000000; i++) {}
+	auto end = std::chrono::high_resolution_clock::now();
+
+	cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();*/
+	
+	//cout << kiko::getFilePath() << endl;
+	//kiko::setFilePath("Assets");
+	//cout << kiko::getFilePath() << endl;
+	//size_t size;
+	//kiko::getFileSize("game.txt", size);
+	//cout << size << endl;
+
+	//std::string s;
+	//kiko::readFile("game.txt", s);
+	//cout << s << endl;
+
+	//kiko::seedRandom((unsigned int) time(nullptr));
+	//for (int i = 0; i < 10; i++) {
+	//	cout << kiko::random(10, 20) << endl;
+	//}
 }
